ada4d32b3018fb07982ec95b63258ef28552e319
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 09:38:37 +0000

    5 types différent de size avec sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int intType;
 	char charType;
+	long long int longLongIntType;
+	long int longIntType;
 	float floatType;
-	double doubleType;
 
-	printf("The size of int:  %d bytes\n", sizeof(intType));
-	printf("The size of int:  %d bytes\n", sizeof(charType));
-	printf("The size of int:  %d bytes\n", sizeof(floatType));
-	printf("The size of int:  %d bytes\n", sizeof(doubleType));
+	printf("The size of int:  %ld bytes\n", sizeof(intType));
+	printf("The size of float: %ld bytes\n", sizeof(floatType));
+	printf("The size of char:  %ld bytes\n", sizeof(charType));
+	printf("The size of long int:  %ld bytes\n", sizeof(longIntType));
+	printf("The size of long long int:  %ld bytes\n", sizeof(longLongIntType));
 	return (0);
 }

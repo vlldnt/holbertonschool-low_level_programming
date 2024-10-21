6e173af16ed2d8be14c982aa27ba8911641b5f8c
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 09:08:12 +0000

    amélioration du code

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - fonction to display text
+ * Return: Zero
+ */
+int main(void)
+{
+	pritnf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

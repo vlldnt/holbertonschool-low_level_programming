9543be0f5eac908c80ccb5260cf6a9c4702efef1
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 14:15:11 +0000

    Print all alphabet with puchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - display al alphabet
+ * Return: O always
+*/
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+		return (0);
+{

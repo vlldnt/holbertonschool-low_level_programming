9a5e1ffcb1ed895ce8977ce7e9e71ac02966b2d6
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 08:46:04 +0000

    remise en plus du comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* Command puts to display text*/
 int main(void)
+/* Command puts to display text*/
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

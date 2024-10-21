3400208c42a61e11870a84e3c676a6ee998eed2d
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 10:15:27 +0000

    number of characters

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int intType;
 	char charType;
-	long long int longLongIntType;
+	int intType;
 	long int longIntType;
+	long long int longLongIntType;
 	float floatType;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(charType));
 	printf("Size of a int: %ld byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longLongIntType));

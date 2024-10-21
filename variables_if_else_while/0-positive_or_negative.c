a045fcc0743f6fc274722c1e530f1f3f46ebe6ae
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 12:42:55 +0000

    Test premier exo

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - négative and positive
+ * Return: O
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{printf("%d is positive\n"), n;
+	}
+	if (n = 0)
+	{printf("%d is zero\n"), n;
+	}	
+	if (n > 0)
+	{printf("%d is negative\n"), n;
+	}
+	return (0);
+}

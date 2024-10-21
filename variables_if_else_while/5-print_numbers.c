a73f6e90fa250e339d917bbb7e9829fa386ad39b
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 14:54:47 +0000

    NUmber 0 to 9 printed

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print 0 to 10
+ * Return: O always
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	{
+		printf("\n");
+	}
+		return (0);
+}

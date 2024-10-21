c5f100c1329972061b9d2499b8c45394b27e3c28
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 12:58:52 +0000

    changement if else is et eme

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	if (n > 0)
+	else
 	{
 		printf("%d is negative\n", n);
 	}

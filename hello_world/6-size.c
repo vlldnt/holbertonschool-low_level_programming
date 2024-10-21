d02b704208d0d845cad4da659f837075de9618f5
vlldnt <vieilledent.adrien@gmail.com>
2024-10-21 10:32:23 +0000

    Pas de declaration que direct le nom du type dans la sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longIntType;
-	long long int longLongIntType;
-	float floatType;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
-	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }

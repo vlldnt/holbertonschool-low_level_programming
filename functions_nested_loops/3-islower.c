b7ac731cc8c158de02cee66ae9dad4e633c8fb20
vlldnt <vieilledent.adrien@gmail.com>
2024-10-22 15:35:47 +0200

    lower upper

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check lower case characters
+ * Return: 1 check is lower or capital
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (0);
+return (1);
+}

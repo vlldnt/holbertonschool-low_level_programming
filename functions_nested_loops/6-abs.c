53c60529fb50651c5b679c5e6b50d0df6d3ebeb4
vlldnt <vieilledent.adrien@gmail.com>
2024-10-22 19:56:03 +0200

    Absolut value of number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absulut value of an interger
+ * @i: declare abs of the number
+ * Return: n or -n for negative number only absolut value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+return (0);
+}

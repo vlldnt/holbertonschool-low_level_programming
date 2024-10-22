f8c6e133da5b5eef5453e831f199d2e7226f5f93
vlldnt <vieilledent.adrien@gmail.com>
2024-10-22 16:34:05 +0200

    minuscules au lieu des majuscules

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 /**
  * _islower - check lower case characters
+ * @c: crotte
  * Return: 1 check is lower or capital
  */
 int _islower(int c)
 {
-	if (c >= 65  && c <= 90)
-		return (0);
-return (1);
+	if (c >= 97  && c <= 122)
+		return (1);
+return (0);
 }

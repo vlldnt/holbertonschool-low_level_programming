cba861787cee913b5d572fd93d13732c2b425ac5
vlldnt <vieilledent.adrien@gmail.com>
2024-10-23 11:37:39 +0200

    again

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,12 @@
 
 int print_last_digit(int n)
 {
-       int l = n % 10;
+	int l = n % 10;
+
 	if (l < 0)
 	{
-		_putchar(-l);
-		return (-l);
-	}
-	else if (l >= 0)
-	{
+		l *= -1;
 		_putchar(l);
-		return (l);
 	}
-	return (0);
+	return (l);
 }

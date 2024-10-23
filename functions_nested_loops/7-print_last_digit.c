16740484433a14f7a021ae990afd4ae8a120e8c9
vlldnt <vieilledent.adrien@gmail.com>
2024-10-23 09:59:05 +0200

    Exo n°7 avec modulus

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit -  print the last digit of a number
+* @n: modulus here
+* Return: is zero here
+*/
+
+int print_last_digit(int n)
+{
+	{
+	if (n < 0)
+		return (-(n % 10));
+	}
+return ((n % 10));
+}

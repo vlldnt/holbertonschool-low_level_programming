b5402ede99013a0a30c7cc78bc1a7dbd54a965b2
vlldnt <vieilledent.adrien@gmail.com>
2024-10-24 16:21:12 +0200

    if dans for dans if avec else

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,21 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-		{
-			n--;
-			printf("%d, ", n);
-		}
+	if (n == 98)
 		printf("%d\n", n);
-	}
 	else
-	{
-		while (n < 98)
-		{
-			n++;
-			printf("%d, ", n);
-		}
-		printf("%d\n", n);
-	}
+		if (n > 98)
+			for (; n >= 98; n--)
+				if (n != 98)
+					printf("%d ,", n);
+				else
+					printf("%d", n);
+		else
+			for (; n <= 98; n++)
+				if (n != 98)
+					printf("%d ,", n);
+				else
+					printf("%d", n);
+	printf("\n");
+
 }

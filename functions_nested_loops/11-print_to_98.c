5b13b62f6d69dc8ec1afd49f75e44a88ffe54d68
vlldnt <vieilledent.adrien@gmail.com>
2024-10-24 16:23:29 +0200

    chnagement espace et virgules

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 		if (n > 98)
 			for (; n >= 98; n--)
 				if (n != 98)
-					printf("%d ,", n);
+					printf("%d, ", n);
 				else
 					printf("%d", n);
 		else
 			for (; n <= 98; n++)
 				if (n != 98)
-					printf("%d ,", n);
+					printf("%d, ", n);
 				else
 					printf("%d", n);
 	printf("\n");

c558a02c6f32b713b25f7acb77421096440b3e2d
vlldnt <vieilledent.adrien@gmail.com>
2024-10-24 09:41:14 +0200

    fuction addition two numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - add two variable together
+* @n1: first number to add
+* @n2: second number to add
+* Return: addition here
+*/
+
+int add(int n1, int n2)
+{
+	int addition;
+
+	addition = n1 + n2;
+return (addition);
+}

269ad0e8e85867c2fab7192cfc72305c7283dd95
vlldnt <vieilledent.adrien@gmail.com>
2024-10-25 09:15:35 +0200

    kk

---
+++ hello_world/100-main.s
@@ -0,0 +1,21 @@
+	.file	"100-main.c"
+	.intel_syntax noprefix
+	.text
+	.ident	"GCC: (Ubuntu 13.2.0-23ubuntu4) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:

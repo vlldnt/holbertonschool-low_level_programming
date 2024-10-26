c7c2fb324739d9c9391f49bc235436a8474a66a7
vlldnt <vieilledent.adrien@gmail.com>
2024-10-26 23:32:58 +0200

    fibonacci sans la derniere virgule

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,20 @@
 
 int main(void)
 {
-	long int n1 = 1;
+	long int n1 = 0;
 	long int n2 = 1;
 	long int n3, n;
 
-	for (n = 1; n <= 50; n++)
+	for (n = 0; n <= 49; n++)
 	{
-		if (n <= 1)
-			n3 = n;
+		n3 = n2 + n1;
+		n1 = n2;
+		n2 = n3;
+
+		if (n == 49)
+			printf("%ld", n3);
 		else
-		{
-			n3 = n2 + n1;
-			n1 = n2;
-			n2 = n3;
-		}
-		printf("%ld, ", n3);
+			printf("%ld, ", n3);
 	}
 	printf("\n");
 return (0);

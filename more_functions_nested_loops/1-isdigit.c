168946e76019cc127fddedd4755a13f87a8ac0cb
vlldnt <vieilledent.adrien@gmail.com>
2024-10-28 08:56:00 +0100

    Is a digit or not

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,11 @@
 /**
 * _isdigit - check for  digit (0 through 9)
 * @c: to be checked
-* Return: 0
+* Return: 0 ans 1 if it's a digit
 */
 int _isdigit(int c)
 {
+	if (c >= 48 && c <= 57)
+		return (1);
 return (0);
 }

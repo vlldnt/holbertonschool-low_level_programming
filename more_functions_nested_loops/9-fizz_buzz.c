3529f839a8422e0a45629bf4a42315f520379e3e
vlldnt <vieilledent.adrien@gmail.com>
2024-10-28 11:09:26 +0100

    task 9 updated

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - buzz fizz exercice
+ * Return: one of the one baby
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0 && n < 100)
+			printf("Buzz ");
+		else if (n % 5 == 0 && n % 3 == 0)
+			printf("FizzBuzz ");
+		else if (n == 100)
+			printf("Buzz\n");
+		else
+			printf("%d ", n);
+	}
+return (0);
+}

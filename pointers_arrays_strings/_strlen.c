4dff907b8ed35c847b440dbe3f5b806bafabeca2
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 10:59:59 +0100

    correction

---
+++ pointers_arrays_strings/_strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strlen - length of a string
+* @s: strings to count
+* Return: num to end the program
+*/
+int _strlen(char *s)
+{
+        int num = 0;
+
+        while (*s)
+        {
+                num++;
+                s++;
+        }
+return (num);
+}

e1a923e4664e81a15cbbe4a12afc4fb93c6a0c2b
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 15:55:52 +0100

    exercice 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strcpy - get stringds and copy then and return them
+* @dest: destination
+* @src: source
+* Return: destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (*src)
+	{
+		len++;
+		src++;
+	}
+
+	src = src - len;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+return (dest);
+}

68c80e6c1149d776788f8a67faa0e964b1ca97de
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 11:09:50 +0100

    again

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -15,10 +15,10 @@ void print_rev(char *s)
 		len++;
 		s++;
 	}
-	for (i = len; i != '\0' - 1; i--)
+	for (i = len; i > 0; i--)
 	{
-		_putchar(*s);
 		s--;
+		_putchar(*s);
 	}
 	_putchar('\n');
 }

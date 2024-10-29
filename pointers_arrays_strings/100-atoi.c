2ed3738c75e15860325d161537e251710a23c12a
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 18:07:02 +0100

    understood for 99.99%, need to knpow break more when needed

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _atoi - les opticiens
+* @s: strings
+* Return:  again 0
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int sum = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			break;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		sum *= 10;
+		sum += *s - '0';
+		s++;
+	}
+
+	if (sign == -1)
+		return (-sum);
+	return (sum);
+}

0b2a68aeb877adfa1b57d3ff7cfc8065de46a8d3
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 10:29:17 +0100

    taks 4 reverse strings

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+* print_rev - okaye
+* @s: coucou
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	for (i = len; i > 0 - 1 ; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}

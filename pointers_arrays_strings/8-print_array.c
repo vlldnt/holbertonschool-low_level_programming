3a801efe221aa401612fdd1789f2175e122eae17
vlldnt <vieilledent.adrien@gmail.com>
2024-10-29 15:10:38 +0100

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - print n number of an array
+* @n: digit
+* @a: first
+* Return: coucou
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+		printf("%d, ", a[i]);
+	}
+}

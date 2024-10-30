ee2c0ab6a56218dd56737d8e0bc40e669173b2a4
vlldnt <vieilledent.adrien@gmail.com>
2024-10-30 14:01:18 +0100

    compare two strings and return

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strcmp - compare two string
+* @s1: compare n°1
+* @s2: compare n°2
+* Return: i dunno
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1 == '\0')
+	{
+		s1++;
+		s2++;
+	}
+return (*s1 - *s2);
+}

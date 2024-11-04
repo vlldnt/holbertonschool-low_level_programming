168d3431f5d8caab5c4c24178d12361c0e68c74b
vlldnt <vieilledent.adrien@gmail.com>
2024-11-04 16:24:15 +0100

    Taks STRPRK

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - locate first occurence in the string
+ * @s: strings
+ * @accept: accepted strings
+ * Return: S
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+	s++;
+	}
+return (0);
+}

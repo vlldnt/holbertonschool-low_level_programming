e302d2078dac72278096b3529b65cee7d9e971ef
vlldnt <vieilledent.adrien@gmail.com>
2024-11-05 15:20:29 +0100

    Task pow recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - raise x of the power y
+ * @x: first number
+ * @y: second number
+ * Return: lower than 0 : return -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+}

41aa7007545ebee7d65782dec5f05325ed28f714
vlldnt <vieilledent.adrien@gmail.com>
2024-11-06 12:01:25 +0100

    Task 0 on argv argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: array containing argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}

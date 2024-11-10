31232e7d1cfff12584f9964addfe8ef2d71b2ef0
vlldnt <vieilledent.adrien@gmail.com>
2024-11-10 18:52:44 +0100

    tet

--- argc_argv/git.c
+++ argc_argv/git.c
@@ -2,37 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    char command[512];
+int main(int argc, char *argv[])
+{
+	char command[512];
+	int i;
+	const char *add = argv[1];
+	char commit[256]= "";
 
-    // Vérifie qu'au moins deux arguments ont été fournis
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <fichiers_a_ajouter> <message_de_commit>\n", argv[0]);
-        return 1;
-    }
-
-    // Le premier argument est pour `git add`
-    const char *add = argv[1];
-
-    // Concatène tous les arguments supplémentaires pour former le message de commit
-    char commit[256] = "";
-    for (int i = 2; i < argc; i++) {
-        strcat(commit, argv[i]);
-        if (i < argc - 1) {
-            strcat(commit, " "); // Ajoute un espace entre les mots du message
-        }
-    }
-
-    // Construit et exécute la commande "git add"
-    snprintf(command, sizeof(command), "git add %s", add);
-    system(command);
-
-    // Construit et exécute la commande "git commit"
-    snprintf(command, sizeof(command), "git commit -m \"%s\"", commit);
-    system(command);
-
-    // Exécute la commande "git push"
-    system("git push");
-
-    return 0;
+	if (argc < 3)
+	{
+		printf("argc[1] is for the file added and argv[2] and more are for the commit\n");
+		return (1);
+	}
+	for (i = 2; i < argc; i++)
+	{
+		strcat(commit, argv[i]);
+		if (i < argc - 1)
+			strcat(commit, " ");
+	}
+	snprintf(command, sizeof(command), "git add %s", add);
+	system(command);
+	snprintf(command, sizeof(command), "git commit -m \"%s\"", commit);
+	system(command);
+	system("git push");
+return (0);
 }

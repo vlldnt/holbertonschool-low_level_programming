f8d916bfc099c355eb578991b867ee166891263d
vlldnt <vieilledent.adrien@gmail.com>
2024-11-10 18:24:05 +0100

    Task numero 4

--- argc_argv/git.c
+++ argc_argv/git.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     char command[512];
 
-    // Vérifie que deux arguments ont été fournis
-    if (argc != 3) {
+    // Vérifie qu'au moins deux arguments ont été fournis
+    if (argc < 3) {
         fprintf(stderr, "Usage: %s <fichiers_a_ajouter> <message_de_commit>\n", argv[0]);
         return 1;
     }
 
-    // Récupère les arguments pour `git add` et `git commit`
+    // Le premier argument est pour `git add`
     const char *add = argv[1];
-    const char *commit = argv[2];
+
+    // Concatène tous les arguments supplémentaires pour former le message de commit
+    char commit[256] = "";
+    for (int i = 2; i < argc; i++) {
+        strcat(commit, argv[i]);
+        if (i < argc - 1) {
+            strcat(commit, " "); // Ajoute un espace entre les mots du message
+        }
+    }
 
     // Construit et exécute la commande "git add"
     snprintf(command, sizeof(command), "git add %s", add);

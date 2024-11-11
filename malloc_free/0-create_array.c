debd6e7b1d9787642a54a8ceb4125d63fe004388
vlldnt <vieilledent.adrien@gmail.com>
2024-11-11 21:46:23 +0100

    Taks 0 corrected

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(sizeof(char) * size);
 	unsigned int i = 0;
 
-	if (size == 0)
+	if (size == 0 || array == 0)
 		return ('\0');
 	for (; i < size ; i++)
 		array[i] = c;

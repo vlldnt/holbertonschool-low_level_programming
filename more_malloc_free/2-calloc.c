09344c69914c9105d73a695b8d6e6c49b5131f83
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 17:09:39 +0100

    task calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,7 +3,27 @@
 #include <stdlib.h>
 /**
  * _calloc - calloc function
- *@nmemb:
- *@size:
+ * @nmemb: i--
+ * @size: --
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *loc;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	loc = malloc(nmemb * size);
+	if (loc == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < nmemb * size)
+	{
+		loc[i] = 0;
+		i++;
+	}
+	return (loc);
+}

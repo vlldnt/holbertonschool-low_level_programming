569452e79ec6207e6c953ba5b547d34fa893e0e6
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 14:05:21 +0100

    Task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,15 +15,16 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = (int **)malloc(sizeof(int **) * height);
+	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 	{
+		free(array);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int *)malloc(sizeof(int *) * width);
+		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (k = 0; k < i; k++)

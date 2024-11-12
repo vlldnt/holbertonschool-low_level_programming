45e3b0d9de156de76aaf9c35705a07ef2d1a5c46
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 14:23:48 +0100

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free the grid
+ * @grid: the grid itself
+ * @height: as said the height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+}

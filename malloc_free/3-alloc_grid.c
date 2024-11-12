786f8a862e28b828e12747f035ddd6a216050a79
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 14:03:28 +0100

    taks with sizeof 0-create_array.c 0-main.c 1-main.c 1-strdup.c 2-main.c 2-str_concat.c 3-alloc_grid.c 3-main.c gitPush main.h prog _putchar.c README.md

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,10 +15,9 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = (int**)malloc(sizeof(int) * height);
+	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 	{
-		free(array);
 		return (NULL);
 	}
 

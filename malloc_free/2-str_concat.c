e605595b19ca319ee2455fddb0598313442c924d
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 11:15:18 +0100

    add concat NULL

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -25,6 +25,9 @@ char *str_concat(char *s1, char *s2)
 		len2++;
 	concat = malloc(sizeof(char) * (len1 + len2) + 1);
 
+	if (concat == NULL)
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j < len2; j++)

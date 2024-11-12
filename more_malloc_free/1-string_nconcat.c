f4d4072fa17cf719c8c55d93e48ca663569dd634
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 16:31:15 +0100

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,3 +9,37 @@
  * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		len2 = n;
+
+	concat = malloc(sizeof(char) * (len1 + len2) + 1);
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		concat[i] = s1[i];
+		for (j = 0; j < n; j++)
+			concat[len1 + j] = s2[j];
+	}
+
+	concat[len1 + len2 + 1] = '\0';
+return (concat);
+}

c35f48a7a387df0d60434d0f39230db78c87e538
vlldnt <vieilledent.adrien@gmail.com>
2024-11-12 10:10:06 +0100

    Task1 strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - change pointer
+ * @str :copy this string
+ * Return: Always 1 or NULL if == 0
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char *temp = str;
+	char *dest;
+
+	while (*temp)
+	{
+		temp++;
+		len++;
+	}
+	dest = malloc(sizeof(char) * len);
+	for (; i < len; i++)
+		dest[i] = str[i];
+	if (len == 0 || str == 0)
+		return ('\0');
+return (dest);
+}

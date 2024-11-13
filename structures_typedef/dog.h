6f83f6b6b19289e1a74bbbc38fc6af81a1f9b5f0
vlldnt <vieilledent.adrien@gmail.com>
2024-11-13 10:59:27 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+/**
+ * struct dog - Struct doggy style
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

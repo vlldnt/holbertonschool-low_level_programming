3485ef8e07031278486e6744df2e4e366ba167e6
vlldnt <vieilledent.adrien@gmail.com>
2024-11-13 11:31:43 +0100

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - my_dog initialization
+ * @d: name of struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}

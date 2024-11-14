780ea4ae5127110d43a4803ef7e68704c10f96b4
vlldnt <vieilledent.adrien@gmail.com>
2024-11-14 13:51:53 +0100

    TasK4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+* _strcpy - get stringds and copy then and return them
+* @dest: destination
+* @src: source
+* Return: destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (*src)
+	{
+		len++;
+		src++;
+	}
+
+	src = src - len;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+return (dest);
+
+}
+/**
  * new_dog - create a new dog
  * @name: name
  * @age: age
@@ -27,7 +54,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new->name = malloc(strlen(name) + 1);
 	new->owner = malloc(strlen(owner) + 1);
 
-	if (new->name == NULL || new->owner == NULL)
+	if (new->name == 0 || new->owner == 0)
 	{
 		free(new->name);
 		free(new->owner);
@@ -35,9 +62,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	strcpy(new->name, name);
+	_strcpy(new->name, name);
 	new->age = age;
-	strcpy(new->owner, owner);
+	_strcpy(new->owner, owner);
 
 return (new);
 }

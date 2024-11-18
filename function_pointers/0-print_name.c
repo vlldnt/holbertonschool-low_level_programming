978600fd100383de4b130572ec1d29b8f1377392
vlldnt <vieilledent.adrien@gmail.com>
2024-11-18 09:56:52 +0100

    TAsk0 Print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,10 +2,12 @@
 #include "function_pointers.h"
 /**
  * print_name - Print *name
- * @name: 
- * af: function pointer
+ * @name: name parameter
+ * @f: function pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
-	*f = &print_name;
+	if (name == NULL || (*f) == NULL)
+		return;
+	(*f)(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,2 +1,9 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+#include "function_pointers.h"
+#include <stdio.h>
+
 void print_name(char *name, void (*f)(char *));
 
+#endif

a0304c18f18546d5cf701eef73aa34bf2d63b517
vlldnt <vieilledent.adrien@gmail.com>
2024-11-20 15:48:52 +0100

    coucou

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Function that returns he sum of all its parameters
+ * @n: 
+ * Return: 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,4 @@
+## Variadic\_Functions
+
+- **[0-sum_them_all.c](https://github.com/vlldnt/holbertonschool-low_level_programming/blob/main/variadic_function/0-sum_tell.c)** Print the sum of all its parameters
+

1949d27e6d40b2502fe21963eb6b153f655d092f
vlldnt <vieilledent.adrien@gmail.com>
2024-11-21 22:42:53 +0100

    Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include "variadic_functions.h"
-#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * pr_char - print char
- * @format: list
+ * pr_char - print characters
+ * @list: the list
  */
 void pr_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
 
-
 /**
- * pr_int - print int
- * @format: list of formats
+ * pr_int - print integer
+ * @list: the list
  */
 void pr_int(va_list list)
 {
@@ -22,41 +21,65 @@ void pr_int(va_list list)
 }
 
 /**
- * pr_string - print string
- * @format: list format
- */
-void pr_string(va_list list)
-{
-	printf("%s", va_arg(list, char*));
-}
-
-/**
  * pr_float - print float
- * @format: list
+ * @list: the list again
  */
 void pr_float(va_list list)
 {
-
+	printf("%f", va_arg(list, double));
 }
 
-/**     
- * print_all - print anything
- * @format: list 
+/**
+ * pr_string - print string
+ * @list: the list itself
+ */
+void pr_string(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char*);
+
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * print_all
+ * @format: list of format checked
  */
 void print_all(const char * const format, ...)
 {
-	type[] = {
-		{'c', pr_char},
-		{'i', pr_int},
-		{'f', pr_float},
-		{'s', pr_string}
+	type form[] = {
+		{"c", pr_char},
+		{"i", pr_int},
+		{"f", pr_float},
+		{"s", pr_string},
 	};
-	int i;
-	va_list print;
 
-	va_start(print, format);
+	va_list printed;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *sep;
 
+	va_start(printed, format);
+	sep = "";
 
-
-	va_end(print);
+	while (format != NULL && format[i] != '\0')
+	{
+		while (j < 4)
+		{
+			if (format[i] == form[j].letter[0])
+			{
+				printf("%s", sep);
+				form[j].function(printed);
+				sep = ", ";
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(printed);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void pr_float(va_list list);
+void pr_int(va_list list);
+void pr_string(va_list list);
+void pr_char(va_list list);
+
+/**
+ * struct type - structure to chekc
+ * @letter: letter
+ * @function: function
+ */
+
+typedef struct type
+{
+	char *letter;
+	void (*function)(va_list print);
+} type;
 
 #endif

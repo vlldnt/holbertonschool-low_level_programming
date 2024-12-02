8175c9bd4f07d8056826312899e503d8e73a9b7e
vlldnt <vieilledent.adrien@gmail.com>
2024-12-02 18:34:23 +0100

    Task 0 same as singly almost

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_dlistint - print elements of a list
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[Ø] (nil)\n");
+		else
+			printf("%d\n", h->len);
+	h = h->next;
+	len++;
+	}
+return (len);
+}

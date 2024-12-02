d3f8d27923428c3a79f70cbf74f47cc4571a7b84
vlldnt <vieilledent.adrien@gmail.com>
2024-12-02 10:38:53 +0100

    Task 0 print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_list - print element of a list
+ * @h: List linked
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[Ø] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	len++;
+	}
+return (len);
+}

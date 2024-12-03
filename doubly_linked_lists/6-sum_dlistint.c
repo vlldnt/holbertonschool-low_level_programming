ac76c191c4b25750651b19558e1040bc6d3a1897
vlldnt <vieilledent.adrien@gmail.com>
2024-12-03 11:00:29 +0100

    Task sum

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * sum_dlistint - sum all the int of the nodes
+ * @head: list of node
+ * Return: 0 if empty or the sum of all
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (2);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+return (sum);
+}

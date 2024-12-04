85eef2fc4e598c615eb7cc1642bf56c6653b1390
vlldnt <vieilledent.adrien@gmail.com>
2024-12-04 15:56:48 +0100

    Task 8 delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -8,7 +8,38 @@
  * @index: index you want to delete
  * Return: 1 if suceed or -1 if failed
  */
-delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	
+	dlistint_t *temp;
+	dlistint_t *node;
+	unsigned int i = 0;
+
+	if (!*head)
+		return (-1);
+	{
+		if (index == 0)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+			return (1);
+		}
+		i++;
+		temp = (*head)->next;
+		node = *head;
+
+		while (temp)
+		{
+			if (index == i)
+			{
+				node->next = temp->next;
+				free(temp);
+				return (1);
+			}
+		node = temp;
+		temp = temp->next;
+		i++;
+		}
+	}
+return (-1);
 }

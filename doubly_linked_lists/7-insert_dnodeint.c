9dba66a9fbfc54c8e777fe189ef3b1f947fad3ac
vlldnt <vieilledent.adrien@gmail.com>
2024-12-04 14:43:56 +0100

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,28 +16,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int i = 0;
 
-	if (*h == NULL || new == NULL)
+	if (h == NULL)
 		return (NULL);
-
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
-
-	while (temp != NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (temp != NULL && i < idx - 1)
 	{
-		if (temp->next == NULL)
-			return (add_dnodeint_end(h, n));
-
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			new->prev = temp;
-			temp->next = new;
-			return (new);
-		}
 		temp = temp->next;
 		i++;
 	}
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new->next = temp->next;
+	new->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new;
+	temp->next = new;
 return (NULL);
 }

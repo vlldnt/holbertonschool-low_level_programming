81ff0b0a3f526d857b7e5661a42b4dfff942c993
vlldnt <vieilledent.adrien@gmail.com>
2024-12-05 11:14:45 +0100

    Read write and open

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file abnd print it
+ * to the `POSIX`standard output
+ * @filename: Name of the file
+ * @letters: letters to print
+ * Return: 0 if failed or number of letter it could read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, len;
+	char buf[512];
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	len = read(file, buf, letters);
+
+	write(1, buf, len);
+return (len);
+}

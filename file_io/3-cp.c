a93a7d211c7a2b13d14384f4d77a40fee2b04671
vlldnt <vieilledent.adrien@gmail.com>
2024-12-05 15:38:13 +0100

    Task 3 - copy file to file

---
+++ file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * main - copy text from file to another file
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: exit 98 if it fails
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from = open(argv[1], O_RDONLY);
+	int file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	char buff[BUFF_SIZE];
+	int text_copy, text_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	text_copy = read(file_from, buff, BUFF_SIZE);
+	if (text_copy == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	text_written = write(file_to, buff, BUFF_SIZE);
+	if (text_written == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", STDERR_FILENO);
+		exit(100);
+	}
+return (0);
+}

7795af8c2745889961836bb5af38ea55c3ab4250
vlldnt <vieilledent.adrien@gmail.com>
2024-12-06 13:10:39 +0100

    task 2 index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - return value of a bit at a given value
+ * @n: number received
+ * @index: index wanted
+ * Return: value of the number at the index
+ */
+
+int get_bit(unsigned long nt n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	n = n >> index;
+	return (n & 1);
+}
+
